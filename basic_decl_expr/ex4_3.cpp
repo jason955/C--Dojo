ac3fd8ecab5e1eb0b71f2c7e11d7d0d541a7ae57
Jason Barrett <jason95@vt.edu>
2024-05-10 10:37:48 -0400

    4142

--- basic_decl_expr/ex4_3.cpp
+++ basic_decl_expr/ex4_3.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+float rectangle_area(float width, float length) 
+{
+    return width * length;
+}
+
 //Exercise 4-3: Write a program to compute the area and circumference of a rectangle 3 inches wide by 5 inches long. What changes must be made to the program so it works for a rectangle 6.8 inches wide by 2.3 inches long?
+int main()
+{
+    cout << "Rectangle area 1 " << rectangle_area(3, 5);
+    cout << "\n";
+    cout << "Rectangle area 2 " << rectangle_area(6.8, 2.3);
+
+}
--- basic_decl_expr/ex4_4.cpp
+++ basic_decl_expr/ex4_4.cpp
@@ -1,1 +1,74 @@
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+void H_ascii_art() 
+{
+    for (int i = 0; i < 7; i++) {
+        if (i == 3) {
+            cout << "*****";
+        }
+        else {
+            cout << "*   *";
+        }
+        cout << "\n";
+    }
+}
+void E_ascii_art() 
+{
+    for (int i = 0; i < 7; i++) {
+        if (i == 0 || i == 3 || i == 6) {
+            cout << "*****";
+        }
+        else {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+}
+void L_ascii_art() 
+{
+    for (int i = 0; i < 7; i++) {
+        if ( i == 6) {
+            cout << "*****";
+        }
+        else {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+}
+void O_ascii_art() 
+{
+    for (int i = 0; i < 7; i++) {
+        if (i == 0 || i == 6) {
+            cout << "*****";
+        }
+        else {
+            cout << "*   *";
+        }
+        cout << "\n";
+    }
+}
+
+void hello_ascii_art() {
+    H_ascii_art();
+    cout << "\n";
+    E_ascii_art();
+    cout << "\n";
+    L_ascii_art();
+    cout << "\n";
+    L_ascii_art();
+    cout << "\n";
+    O_ascii_art();
+}
+
 //Exercise 4-4: Write a program to print "HELLO" in big block letters where each letter is 7 characters high and 5 characters wide.
+int main()
+{
+    hello_ascii_art();
+}
